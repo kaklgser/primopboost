527447198b6360885a36aca546d498528bf8ec52
kaklgser <primoboostai@gmail.com>
2025-07-26 09:40:58 +0530

    Updated ResumeOptimizer.tsx

--- src/components/ResumeOptimizer.tsx
+++ src/components/ResumeOptimizer.tsx
@@ -1432,14 +1432,7 @@ const ResumeOptimizer: React.FC<ResumeOptimizerProps> = ({
             {optimizedResume && activeTab === 'analysis' && (
               <>
                 {beforeScore && afterScore && initialResumeScore && finalResumeScore && (
-                  <div className="bg-white rounded-xl shadow-lg border border-gray-200 overflow-hidden mb-6">
-                   
-                   
-                        
-                      <div className="mt-6 text-center">
-
-                      </div>
-                    </div>
+                  
                   
                 )}
 
